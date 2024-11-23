a64ef86dac7a76838c23d599240aef3fc86152b3
Xavier <xavierjiang04@gmail.com>
2024-11-23 19:05:35 +0100

    todo edit-button

--- to_do_list/main.js
+++ to_do_list/main.js
@@ -21,9 +21,34 @@ function addToDo() {
         list.removeChild(listItem);
     });
 
-    
+    //Erstellt ein edit Button zum Bearbeiten
+    const editButton = document.createElement('button');
+    editButton.innerHTML = "edit";
+    editButton.className = "edit-button"; // gibt editButton eine Klasse
+    editButton.addEventListener('click', liToList)
 
-    listItem.appendChild(deleteButton);
+    function liToList() {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = listItem.textContent; // li-Item Text in das Textfeld einfügen
+
+        list.replaceChild(input, listItem);
+
+        input.addEventListener("blur", function() {
+            listItem.textContent = input.value;
+            addButtons(listItem);
+            list.replaceChild(listItem, input);
+          });
+    }
+
+    //Buttons hinzufügen
+    function addButtons(item) {
+        item.innerHTML = note;
+        item.appendChild(editButton);
+        item.appendChild(deleteButton);
+    }
+
+    addButtons(listItem);
 
     list.appendChild(listItem);
     document.querySelector('#text').value = '';
