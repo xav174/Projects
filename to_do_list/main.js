b06124977a93ec82e02354e1b5d8e8790d97932f
Xavier <xavierjiang04@gmail.com>
2024-11-26 22:27:36 +0100

    v1 fast fertig

--- to_do_list/main.js
+++ to_do_list/main.js
@@ -1,43 +1,43 @@
 const btn = document.getElementById("addButton");
 const input = document.getElementById("text")
+const listContainer = document.getElementById("listOfItems"); 
 btn.addEventListener('click', addToDo);
 
 function addToDo() {
-
-    const note = document.querySelector('#text').value;
-    if (note.trim() === '') return // Leere Einträge werden ignoriert
-    const list = document.querySelector('ul'); 
-    
-    // Listen Element erstellen
-    const listItem = document.createElement('li');
-    listItem.textContent = note;
-
-    // Button zum löschen erstellen
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = "&#10008;";
-
-    deleteButton.className = "delete-button"; //gibt deleteButton eine Klasse 
-
-    //Beim klicken löschen vom listItem
-    deleteButton.addEventListener('click', () => {
-        list.removeChild(listItem);
-    });
-
-    listOfItems.addEventListener('click', function(e){
-        if (e.target.tagName === 'LI') {
-            e.target.classList.toggle("checked");
-        }
-    }); 
-
-    //Buttons hinzufügen
-    function addButtons(item) {
-        item.innerHTML = note;
-        item.appendChild(deleteButton);
+    if(input.value === ""){
+        alert("You must write something");
     }
-
-    addButtons(listItem);
-
-    list.appendChild(listItem);
+    else{
+        // Listen Element erstellen
+        let listItem = document.createElement('li');
+        listItem.innerHTML = input.value;
+        listContainer.appendChild(listItem);
+    
+        // Button zum löschen erstellen
+        let deleteButton = document.createElement('button');
+        deleteButton.innerHTML = "&#10008;";
+        deleteButton.className = "delete-button"; //gibt deleteButton eine Klasse 
+        listItem.appendChild(deleteButton);
+    }
     input.value = '';
-
+    saveData();
 }
+
+listContainer.addEventListener('click', function(e){
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "BUTTON"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false); 
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+ }
+
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("data");
+ }
+showList();
