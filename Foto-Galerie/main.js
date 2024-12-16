c9ddddac1567dc61f0e061a643c76f25c9d9723b
Xavier <xavierjiang04@gmail.com>
2024-12-16 21:37:53 +0100

    Galerie Animation

--- Foto-Galerie/main.js
+++ Foto-Galerie/main.js
@@ -1,4 +1,4 @@
-const home = document.querySelectorAll(".home-wrapper");
+const fadeElements = document.querySelectorAll(".fade-animation");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
@@ -6,9 +6,24 @@ const observer = new IntersectionObserver((entries) => {
     entry.target.classList.toggle("show", entry.isIntersecting) 
   });  
 }, {
-  threshold: 0.7,
+  threshold: 0.8,
 });
 
-home.forEach(home => {
-  observer.observe(home);
-})
+fadeElements.forEach(fadeElements => {
+  observer.observe(fadeElements);
+});
+
+const fadeElements2 = document.querySelectorAll(".fade-animation2");
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    // Wenn unser Element im Sichtbereich ist (entry.isIntersecting === true), dann wird die Klasse show hinzugefügt, ansonsten wird die Klasse show entfernt (entry.isIntersecting === false)
+    entry.target.classList.toggle("show2", entry.isIntersecting) 
+  });  
+}, {
+  threshold: 0.5,
+});
+
+fadeElements2.forEach(fadeElements2 => {
+  observer2.observe(fadeElements2);
+});
