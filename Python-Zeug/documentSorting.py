1ce7805b039d8c2e10e30bcf6a25b3bc54306f62
Xavier <xavierjiang04@gmail.com>
2025-05-01 17:56:37 +0200

    Dokumente für andere Dateitypen sortieren

--- Python-Zeug/documentSorting.py
+++ Python-Zeug/documentSorting.py
@@ -1,22 +1,46 @@
 import os
 import shutil
 
-current_directory = os.getcwd()
-print(current_directory)
+datei_typ = {
+    ".pdf": "PDF_Dateien",
+    ".jpg": "Bilder",
+    ".jpeg": "Bilder",
+    ".docx": "Office_Dokumente",
+    ".pptx": "Office_Dokumente",
+    ".xlsx": "Office_Dokumente",
+    ".zip": "Zip_Dateien",
+    ".scala": "Scala_Dateien",
+    ".py": "Python_Dateien",
+    
+}
 
 ordner_pfad = "/Users/xavierjiang/Downloads"
-zielordner = os.path.join(ordner_pfad, "PDF_Dateien")
+
 #Alle Dateien im Ordner auflisten
 dateien = os.listdir(ordner_pfad)
 
-#Ordner erstellen falls noch nicht vorhanden
-if not os.path.exists(zielordner):
-    os.mkdir(zielordner)
-    print("Zielordner wurde erstellt")
 
-#Dateien verschieben
 for datei in dateien:
-    if datei.lower().endswith(".pdf"):
+    
+    dateipfad = os.path.join(ordner_pfad, datei)
+    
+    #Ignorieren falls ein Ordner
+    if os.path.isdir(dateipfad):
+        continue
+    
+    #Dateityp bestimmen
+    root, ext = os.path.splitext(datei)
+    ext = ext.lower()
+    
+    #Prüfen ob Dateityp im Dictonary vorhanden
+    if ext in datei_typ:
+        zielordner = os.path.join(ordner_pfad, datei_typ[ext])
+        
+        #Zielordner erstellen falls noch nicht vorhanden
+        if not os.path.exists(zielordner):
+            os.mkdir(zielordner)
+        
+        #Datei in Zielordner verschieben
         quellpfad = os.path.join(ordner_pfad, datei)
         zielpfad = os.path.join(zielordner, datei)
         
