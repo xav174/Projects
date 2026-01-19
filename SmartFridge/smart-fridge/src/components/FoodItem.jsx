4416f765c298aceb6c06d71adf62fc99272f4c33
Xavier Jiang <xavierjiang04@gmail.com>
2026-01-19 18:07:19 +0100

    SmartFridge Anzeige der übrigen Tage mit Farbe

--- SmartFridge/smart-fridge/src/components/FoodItem.jsx
+++ SmartFridge/smart-fridge/src/components/FoodItem.jsx
@@ -21,6 +21,21 @@ const FoodItem = ({ item, onDelete}) => {
   const remainingDays = calculateDaysRemaining(item.expiryDate);
   console.log(remainingDays);
 
+  let statusColor = '#0fb86f';
+  let statusText = `Noch ${remainingDays} Tage haltbar`;
+
+  if (remainingDays <= 0) {
+    statusColor = '#ef4444';
+    statusText = 'Abgelaufen';
+  } else if (remainingDays <= 3) {
+    statusColor = 'orange';
+    statusText = 'Läuft bald ab';
+  }
+
+  const dynamicStyle = {
+    color: statusColor
+  }
+
   return (
     <>
       <div className="item-card">
@@ -32,8 +47,8 @@ const FoodItem = ({ item, onDelete}) => {
           </div>
         </div>
         <div className="item-status">
-          <span className="tag">Abgelaufen</span>
-          <p className="status-date">Gestern</p>
+          <span className="tag" style={dynamicStyle}>{statusText}</span>
+          <p className="status-date">{remainingDays}</p>
         </div>
       </div> 
     </>
